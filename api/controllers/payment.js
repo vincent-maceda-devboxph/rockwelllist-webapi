bd467e211336a1ef03ec331fef176b7ecf8c053d
Vincent Maceda <vincent.maceda@bcstechnology.com>
2018-11-19 19:14:12 +0800

    Fix for payment amount greater than the wallent balance.

--- api/controllers/payment.js
+++ api/controllers/payment.js
@@ -138,7 +138,7 @@ async function getWalletAmount(wallet){
     });
 
     payment.forEach(element => {
-        if(element.amount != undefined)
+        if(element.amount != undefined && element.status != "PENDING" && element.status != "UNSUCCESSFUL")
             totalPayment += element.amount;
     });
 
--- api/controllers/wallet.js
+++ api/controllers/wallet.js
@@ -271,7 +271,7 @@ async function getWalletAmount(wallet){
     });
 
     payment.forEach(element => {
-        if(element.amount != undefined)
+        if(element.amount != undefined && element.status != "PENDING" && element.status != "UNSUCCESSFUL")
             totalPayment += element.amount;
     });
 
